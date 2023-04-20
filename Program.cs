86a703519f16565bcdfcb26b3fdf53f76e3a8beb
Rajat <rajatprajapati09@gmail.com>
2023-04-20 18:22:45 +0530

    [rajat]stack and Queue program file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stacks and Queues");
+        }
+    }
+}
